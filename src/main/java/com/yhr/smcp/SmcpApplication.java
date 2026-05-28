6a752ad8c8a624854889877fda0e8daad73b13b0
Daniel <danvalleribeiro@gmail.com>
2026-05-28 17:58:34 -0300

    Initial Setup

---
+++ src/main/java/com/yhr/smcp/SmcpApplication.java
@@ -0,0 +1,13 @@
+package com.yhr.smcp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmcpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yhr/smcp/SmcpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yhr.smcp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
