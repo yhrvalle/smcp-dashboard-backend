f5424f783ce0247a9ae83c76e4c67db39ac49093
Daniel <danvalleribeiro@gmail.com>
2026-07-08 03:37:05 -0300

    wip: guildMember mapper

---
+++ src/main/java/com/yhr/smcp/mappers/GuildMapper.java
@@ -0,0 +1,18 @@
+package com.yhr.smcp.mappers;
+
+import com.yhr.smcp.dto.response.guild.GuildMemberDTO;
+import com.yhr.smcp.entities.guild.GuildMember;
+
+public class GuildMapper {
+    public static GuildMemberDTO toGuildMemberDTO(GuildMember guildMember, String className, String specName, String race) {
+        return new GuildMemberDTO(
+                guildMember.getName(),
+                guildMember.getRealm(),
+                guildMember.getLevel(),
+                className,
+                specName,
+                race
+        );
+
+    }
+}
