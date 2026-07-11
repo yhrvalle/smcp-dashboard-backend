8605e4ac1711dcb93c486cfeb6a35f13f9d7cf68
Daniel <danvalleribeiro@gmail.com>
2026-07-11 09:16:09 -0300

    wip: added sync custom exception and removed character sync flow from guildroster sync

---
+++ src/main/java/com/yhr/smcp/exceptions/BlizzardSyncException.java
@@ -0,0 +1,7 @@
+package com.yhr.smcp.exceptions;
+
+public class BlizzardSyncException extends RuntimeException {
+    public BlizzardSyncException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
--- src/main/java/com/yhr/smcp/services/guild/GuildService.java
+++ src/main/java/com/yhr/smcp/services/guild/GuildService.java
@@ -2,6 +2,7 @@ package com.yhr.smcp.services.guild;
 
 import com.yhr.smcp.entities.guild.Guild;
 import com.yhr.smcp.entities.guild.GuildMember;
+import com.yhr.smcp.exceptions.BlizzardSyncException;
 import com.yhr.smcp.parsers.guild.GuildParser;
 import com.yhr.smcp.parsers.guild.GuildRosterParser;
 import com.yhr.smcp.repositories.guild.GuildMemberRepository;
@@ -33,30 +34,36 @@ public class GuildService {
     private Integer maxRank;
 
     public Guild syncGuild(String realm, String guildSlug) {
-        String rawJson = blizzardApiService.getGuild(realm, guildSlug).block();
-        JsonNode guildRoot = objectMapper.readTree(rawJson);
-        Guild guild = guildParser.parse(guildRoot);
-        guildRepository.save(guild);
-        syncGuildRoster(realm, guildSlug, guild); //TODO: maybe tirar isso daqui pro sync ser só dos dados da guild ai ser outro endpoint que puxa o roster
+        Guild guild = fetchGuildAndSave(realm, guildSlug);
+        syncGuildRoster(realm, guildSlug, guild);
         return guild;
     }
 
+    private Guild fetchGuildAndSave(String realm, String guildSlug) {
+        try {
+            String rawJson = blizzardApiService.getGuild(realm, guildSlug).block(); // checar se esse metodo da blizzard aceita guildId
+            JsonNode guildRoot = objectMapper.readTree(rawJson);
+            Guild guild = guildParser.parse(guildRoot);
+            return guildRepository.save(guild);
+        } catch (Exception e) {
+            throw new BlizzardSyncException("Failed to sync guild=%s, at realm=%s".formatted(guildSlug, realm), e);
+        }
+    }
+
     private void syncGuildRoster(String realm, String guildSlug, Guild guild) {
-        String rawJson = blizzardApiService.getGuildRoster(realm, guildSlug).block();
-        JsonNode rosterRoot = objectMapper.readTree(rawJson);
-        List<GuildMember> guildMembers = guildRosterParser.parse(rosterRoot, maxRank);
-        for (GuildMember guildMember : guildMembers) {
-            if (guildMemberRepository.existsById(guildMember.getId())) {
-                continue;
-            }
-            try {
+        try {
+            String rawJson = blizzardApiService.getGuildRoster(realm, guildSlug).block();
+            JsonNode rosterRoot = objectMapper.readTree(rawJson);
+            List<GuildMember> guildMembers = guildRosterParser.parse(rosterRoot, maxRank);
+            for (GuildMember guildMember : guildMembers) {
+                if (guildMemberRepository.existsById(guildMember.getId())) {
+                    continue;
+                }
                 guildMember.setGuild(guild);
                 guildMemberRepository.save(guildMember);
-                characterService.syncCharacter(guildMember);
-
-            } catch (Exception e) {
-                log.error("failed sync member={} id={}: {}", guildMember.getName(), guildMember.getId(), e.getMessage());
             }
+        } catch (Exception e) {
+            throw new BlizzardSyncException("Failed to sync guild roster, guild=%s, at realm=%s".formatted(guildSlug, realm), e);
         }
 
     }
